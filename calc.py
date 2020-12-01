415f35057507b3d0a98bdcaed71485503c80cde5
Zach Bouvier <zbouvier99@gmail.com>
2020-12-01 10:30:10 -0600

    added comments

--- calc.py
+++ calc.py
@@ -3,14 +3,14 @@ def day1():
     f = open('values.txt', 'r+')
     lines = []
     for line in f.readlines():
-        lines.append(int(line.rstrip()))
+        lines.append(int(line.rstrip())) #remove those pesky new lines
     f.close()
     #print(lines)
     for val in lines:
         for val2 in lines:
             for val3 in lines:
                 if(val + val2 + val3 == 2020):
-                    return(str(val*val2*val3))
+                    return(str(val*val2*val3)) #definitely not optimal -- can ignore a bunch of elements / not use 3 for loops
 
 print(day1())
          
